5559c5bc036693a9e540cdb7056e4a9c4fb235e2
jorosoft <jorosoftbg@gmail.com>
2017-05-08 16:40:32 +0300

    fix testing configuration

--- tests/systemjs-config.js
+++ tests/systemjs-config.js
@@ -3,7 +3,6 @@ SystemJS.config({
     map: {
         "plugin-babel": "../node_modules/systemjs-plugin-babel/plugin-babel.js",
         "systemjs-babel-build": "../node_modules/systemjs-plugin-babel/systemjs-babel-browser.js",
-        "models": "../scripts/models/models.js",
         "tests": "./tests.js"
     }
 });
--- tests/tests.js
+++ tests/tests.js
@@ -1,10 +1,10 @@
-// import { vehicleController } from '../scripts/controllers/vehicle-controller.js';
-// import { templateLoader } from '../scripts/common/template-loader.js'
-// import { userController } from '../scripts/controllers/user-controller.js';
-// import * as models from '../scripts/models/models.js';
+import { vehicleController } from '../scripts/controllers/vehicle-controller.js';
+import { templateLoader } from '../scripts/common/template-loader.js'
+import { userController } from '../scripts/controllers/user-controller.js';
+import * as models from '../scripts/models/models.js';
 
 mocha.setup('bdd');
-expect = chai.expect;
+const expect = chai.expect;
 
 // const TEST_TEMPLATE = Handlebars.complile("<div></div>")
 // const TEST_USER = 'test_user';
@@ -13,10 +13,10 @@ describe("Our App Tests", function() {
 
     describe("Models Tests", function() {
         it("shouldCreateCar", function() {
-            //let car = models.getCar("BMW", "330", 2008, "petrol", 300, 8800, "NoInfo");
+            let car = models.getCar("BMW", "A330", 2008, "petrol", 300, 8800, "NoInfo");
 
-            expect(10).to.be.above(5);
-            // expect('foobar').to.contain('petrol');
+            expect(car.make).to.equal("BMW");
+
         });
     });
 
