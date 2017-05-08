f64cc9ad61ec86c058523f2db8e094ced7b5131b
VVoev <vladovoev@yahoo.co.uk>
2017-05-08 17:39:35 +0300

    added some trivial Unit Tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,6 +1,6 @@
-import { vehicleController } from '../scripts/controllers/vehicle-controller.js';
-import { templateLoader } from '../scripts/common/template-loader.js'
-import { userController } from '../scripts/controllers/user-controller.js';
+import {vehicleController} from '../scripts/controllers/vehicle-controller.js';
+import {templateLoader} from '../scripts/common/template-loader.js'
+import {userController} from '../scripts/controllers/user-controller.js';
 import * as models from '../scripts/models/models.js';
 
 mocha.setup('bdd');
@@ -9,20 +9,68 @@ const expect = chai.expect;
 // const TEST_TEMPLATE = Handlebars.complile("<div></div>")
 // const TEST_USER = 'test_user';
 
-describe("Our App Tests", function() {
+describe("Our App Tests", function () {
 
-    describe("Models Tests", function() {
-        it("shouldCreateCar", function() {
-            let car = models.getCar("BMW", "A330", 2008, "petrol", 300, 8800, "NoInfo");
+    describe("Models Tests", function () {
 
-            expect(car.make).to.equal("BMW");
-
+        describe("Car Tests", function () {
+            it("MakerToBeBmw_WhenCreatingCarBmw", function () {
+                let car = models.getCar("BMW", "330i", 2008, "petrol", 300, 8800, "NoInfo");
+                expect(car.make).to.equal("BMW");
+            });
+            it("ModelToBe330i_WhenCreatingCarWithModel330i", function () {
+                let car = models.getCar("BMW", "330i", 2008, "petrol", 300, 8800, "NoInfo");
+                expect(car.model).to.equal("330i");
+            });
+            it("CarFuelTypeToBePetrol_WhenCreatingCarWithFuelTypePetrol", function () {
+                let car = models.getCar("BMW", "330i", 2008, "petrol", 300, 8800, "NoInfo");
+                expect(car.fuelType).to.equal("petrol");
+            });
+            it("CarHorsePowersToBe300_WhenCreatingCarWith300HorsePowers", function () {
+                let car = models.getCar("BMW", "330i", 2008, "petrol", 300, 8800, "NoInfo");
+                expect(car.fuelType).to.equal("petrol");
+            })
+            it("CarInfoToBeEqualToAdded_Value", function () {
+                let car = models.getCar("BMW", "330i", 2008, "petrol", 300, 8800, "ValueForTheMoney");
+                expect(car.info).to.equal("ValueForTheMoney");
+            })
         });
+
+        describe("Bus Tests", function () {
+            it("BusMakerToBeIkarus_WhenMakerIsICarus",function () {
+                let bus = models.getBus("Ikarus","440x",2010,"diesel",450,99000,"PartyBus",44);
+                expect(bus.make).to.equal("Ikarus");
+            })
+            it("BusModelToBe440x_WhenModels440x",function () {
+                let bus = models.getBus("Ikarus","440x",2010,"diesel",450,99000,"PartyBus",44);
+                expect(bus.make).to.equal("Ikarus");
+            })
+            it("BusFuelTypeToBeDiesel_WhenIsDiesel",function () {
+                let bus = models.getBus("Ikarus","440x",2010,"diesel",450,99000,"PartyBus",44);
+                expect(bus.make).to.equal("Ikarus");
+            })
+            it("BusPriceToBe990000_WhenPriceIs99000Euro",function () {
+                let bus = models.getBus("Ikarus","440x",2010,"diesel",450,99000,"PartyBus",44);
+                expect(bus.make).to.equal("Ikarus");
+            })
+            it("BusMakerToBeChanged_WhenMakerIsChanged",function () {
+                let bus = models.getBus("Ikarus","440x",2010,"diesel",450,99000,"PartyBus",44);
+                bus.make = "Man";
+                expect(bus.make).to.equal("Man");
+            })
+        });
+
+        describe("User Tests", function () {
+           it("ExpectNewUSerToBeCreatedSuccesfully_WhenCreatingNewUser",function () {
+               let user = models.getNewUser("Vlado","1","vlado@abv.bg");
+           })
+        });
+
+
+
+
     });
 
-    describe("Controllers Tests", function() {
-
-    });
 });
 
 mocha.run();
